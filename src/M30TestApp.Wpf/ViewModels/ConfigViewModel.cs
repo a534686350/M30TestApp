5b071e8f185e73237b64dabbe1f74b6d23195178
a534686350 <a534686350@users.noreply.github.com>
2026-06-08 14:01:44 +0800

    Bump version to v1.2.4

--- src/M30TestApp.Wpf/ViewModels/ConfigViewModel.cs
+++ src/M30TestApp.Wpf/ViewModels/ConfigViewModel.cs
@@ -345,7 +345,7 @@ public sealed class ConfigViewModel : ViewModelBase
         get
         {
             var v = Assembly.GetEntryAssembly()?.GetName().Version;
-            return v is null ? "1.2.3" : $"{v.Major}.{v.Minor}.{v.Build}";
+            return v is null ? "1.2.4" : $"{v.Major}.{v.Minor}.{v.Build}";
         }
     }
     public string Changelog { get; }
